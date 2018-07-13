7a48b50dfb153189f510dd04a7eaa561a8627b9f
blueudp <41192980+blueudp@users.noreply.github.com>
2018-07-13 16:38:52 +0100

    Update dorkdef.py

--- dorkdef.py
+++ dorkdef.py
@@ -108,8 +108,8 @@ class dork:
                     if 'low' in Impact.lower():
                         Impacts = "\033[1;32;40mlow\033[0m"
                             
-                    resultone = "[#] Found: " + j.encode('UTF-8')  + "\nImpact: " + Impact + "Description: " + Description + "\n"# without color, (log)   
-                    resultones = "[#] Found: " + j.encode('UTF-8')  + "\nImpact: " + Impacts  + "\n" + "Description: " + Description #with color, (terminal), the diference between resultone and resultones is Impact variable, with color and colorblind
+                    resultone = "[#] Found: " + j.link.encode('UTF-8')  + "\nImpact: " + Impact + "Description: " + Description + "\n"# without color, (log)   
+                    resultones = "[#] Found: " + j.link.encode('UTF-8')  + "\nImpact: " + Impacts  + "\n" + "Description: " + Description #with color, (terminal), the diference between resultone and resultones is Impact variable, with color and colorblind
  
                     print(resultones) #print results
                     with open(name_file, 'a') as report_file:
